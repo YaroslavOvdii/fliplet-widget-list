6f79879f27b74cc517c3e9ce577f72aaa816b77d
YaroslavOvdii <yaroslav.o@upplabs.com>
2019-12-24 10:09:43 +0200

    Changed to instance

--- js/build.js
+++ js/build.js
@@ -1,23 +1,30 @@
-$('.linked[data-list-item-id]').click(function (event) {
-  event.preventDefault();
+(function () {
+  $('.linked[data-list-item-id]').click(function (event) {
+    event.preventDefault();
 
-  if ($(this).parents('.list-swipe.swiping').length) {
-    return;
-  }
+    var $container = $(this);
 
-  var data = Fliplet.Widget.getData($(this).parents('[data-list-id]').attr('data-list-id'));
-  var itemData = _.find(data.items,{id: $(this).attr('data-list-item-id')});
+    if ($container.parents('.list-swipe.swiping').length) {
+      return;
+    }
 
-  if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
-    Fliplet.Navigate.to(itemData.linkAction);
-  }
-});
+    var data = Fliplet.Widget.getData($container.parents('[data-list-id]').attr('data-list-id'));
+    var itemData = _.find(data.items, {
+      id: $container.attr('data-list-item-id')
+    });
 
-$('[data-list-id]').each(function(){
-  var data = Fliplet.Widget.getData( $(this).attr('data-list-id') );
+    if (!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+      Fliplet.Navigate.to(itemData.linkAction);
+    }
+  });
+})();
+
+Fliplet.Widget.instance('list', function (data) {
+  var $container = $(this);
+
   if (data.swipeToSave) {
     window.ui = window.ui || {};
-    window.ui['swipeSavedList' + $(this).attr('data-list-uuid')] = new SwipeSaveList(this, {
+    window.ui['swipeSavedList' + $container.attr('data-list-uuid')] = new SwipeSaveList(this, {
       savedListLabel: data.swipeToSaveLabel || 'My list'
     });
   }
