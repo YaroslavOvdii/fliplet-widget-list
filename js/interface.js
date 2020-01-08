6224b3c8626e588642c9adbf0334d9a8ebc96164
YaroslavOvdii <yaroslav.o@upplabs.com>
2020-01-08 12:30:18 +0200

    chabged checkPanelLength function

--- js/interface.js
+++ js/interface.js
@@ -269,7 +269,7 @@ function addListItem(data) {
 }
 
 function checkPanelLength() {
-  if ($('.panel').length) {
+  if (data.items.length) {
     $('#list-items').removeClass('list-items-empty');
   } else {
     $('#list-items').addClass('list-items-empty');
